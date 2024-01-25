251d6be23850db87536d1236306c255138848f45
SamuelGonzalez2799 <91762236+SamuelGonzalez2799@users.noreply.github.com>
2024-01-25 15:47:56 -0600

    Delete nm2pixels.m

--- nm2pixels.m
+++
@@ -1,9 +0,0 @@
-function pixLocs = nm2pixels(locs,pixSize)
-
-if(~(numel(pixSize) == 1 && isreal(pixSize) && pixSize>0))
-    error('pixSize must be a positive real scalar value.');
-end
-
-pixLocs = locs/pixSize;
-
-end
