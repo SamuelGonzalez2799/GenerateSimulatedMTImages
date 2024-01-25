5bc5524a9527323daa7f14bda4f1339733f8fe7e
SamuelGonzalez2799 <91762236+SamuelGonzalez2799@users.noreply.github.com>
2024-01-25 14:17:24 -0600

    Add files via upload
    adding the script and dependencies

---
+++ Gonzalez_simulated_MT_growthForEvL.m
@@ -0,0 +1,35 @@
+%This is to take data from the EB1 dynamics model and then plot it to
+%create a video showing the growth and then you can create a kymograph from
+%it. 
+ %addpath('Z:\cbs_lab_klei0091\Sam\Taylor code for MT dynamics with tpx2\MicroTubule\ImageSim\ImageSim');
+ %addpath('Z:\cbs_lab_klei0091\Sam\Taylor code for MT dynamics with tpx2\MicroTubule\Include');
+ %addpath('R:\cbs_lab_klei0091\Sam\Taylor code for MT dynamics with tpx2\MicroTubule\ImageSim\ImageSim');
+% addpath('R:\cbs_lab_klei0091\Sam\Taylor code for MT dynamics with tpx2\MicroTubule\Include');
+%addpath('C:\Users\samue\Documents\umn\employment\SimualtionData9-15-22\CodeToRunSimsLocally\Include');
+%addpath('C:\Users\samue\Documents\umn\employment\SimualtionData9-15-22\CodeToRunSimsLocally\ImageSim\ImageSim');
+for i=1
+   
+    index=1;
+    number=num2str(i);
+    proteinLocations=readmatrix('proteinBindingAndRemovalsWithEvL280StandardKineticsForPercentEB1AtEdge.xlsx','Sheet',i);
+    protoLengths=readmatrix('protofilamentLengths280StandardKineticsForPercentEB1AtEdge.xlsx','Sheet',i);
+ videoOutputName=strcat('protofilamentLengths280StandardKineticsForPercentEB1AtEdge',num2str(i),'-20FPSBackground1For64nMPixelsEvery5Frames.avi');%Give the name for the output video
+ aviobj1 = VideoWriter(videoOutputName, 'Uncompressed AVI');
+    aviobj1.FrameRate = 20;  %frame rate in frames/sec
+   
+    open(aviobj1);  %now opening the avi object
+                        for j=1:5:size(protoLengths,1)-1%-1 is because protolengths has titles in row 1
+                            proteins=proteinLocations(:,3*j-2:3*j);
+                           outputImage=ImageSimControllerFuncEvL(proteins,protoLengths(j+1,1:13));%plus 1 is cause protolenghs has titles in row 1
+                            figure;
+%         subPlot(1,1,1);
+        imshow(mat2gray(outputImage)) ;
+                            frame = getframe(gcf);   %captures CURRENT image
+    writeVideo(aviobj1,frame);
+                            close()
+                        end
+                        close(aviobj1);
+                        index=index+1;
+                        close all;
+                        
+end
